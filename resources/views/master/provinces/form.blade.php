ec71798042f5eb0d5eb3e29b5d4c3ab06efd9052
cakanang16 <anangeko9@gmail.com>
2025-02-10 15:00:43 +0700

    first commit

--- resources/views/master/provinces/form.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="mb-3">
-    <label class="form-label required">Nama Provinsi</label>
-    <input type="text" name="name" class="form-control @error('name') is-invalid @enderror"
-           value="{{ old('name', $province->name ?? '') }}" {{ isset($viewMode) && $viewMode ? 'disabled' : '' }} required>
-    @error('name')
-    <div class="invalid-feedback">{{ $message }}</div>
-    @enderror
-</div>
